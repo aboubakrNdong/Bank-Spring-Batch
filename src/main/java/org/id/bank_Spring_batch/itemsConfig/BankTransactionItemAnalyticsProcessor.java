e33525e1f025d2c0dbaf67d66b59980c69da6f94
aboubakrNdong <aboubakr.ndong@gmail.com>
2025-03-26 17:12:55 +0100

    feat(Processor): Create BankTransactionItemAnalytics for composite Item Processors

---
+++ src/main/java/org/id/bank_Spring_batch/itemsConfig/BankTransactionItemAnalyticsProcessor.java
@@ -0,0 +1,28 @@
+package org.id.bank_Spring_batch.itemsConfig;
+
+
+import org.id.bank_Spring_batch.model.BankTransaction;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+import lombok.Getter;
+
+@Component
+public class BankTransactionItemAnalyticsProcessor implements ItemProcessor<BankTransaction, BankTransaction> {
+
+    @Getter
+    private double totalDebit;
+
+    @Getter
+    private double totalCredit;
+
+
+    @Override
+    public BankTransaction process(BankTransaction bankTransaction) throws Exception {
+
+        if(bankTransaction.getTransactionType().equals("D"))
+            totalDebit += bankTransaction.getAmount();
+        else if (bankTransaction.getTransactionType().equals("C"))
+            totalCredit += bankTransaction.getAmount();
+        return bankTransaction;
+    }
+}
